5ae04692f343850ab01fff8e7a1cc82349aa5e8d
Anshuman Rana <anshumanrana331@gmail.com>
2021-03-22 13:41:25 +0530

    Updates

---
+++ precache-manifest.2115b2f7378d9b60de542e41cc28f764.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77dc3b9550a007b07d606ba2cafff80f",
+    "url": "/Firegram-Gallery-App/index.html"
+  },
+  {
+    "revision": "4171a13325f535de19eb",
+    "url": "/Firegram-Gallery-App/static/css/main.143a44ac.chunk.css"
+  },
+  {
+    "revision": "fd28b51d053389422483",
+    "url": "/Firegram-Gallery-App/static/js/2.a2c99230.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/Firegram-Gallery-App/static/js/2.a2c99230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4171a13325f535de19eb",
+    "url": "/Firegram-Gallery-App/static/js/main.e1a0a490.chunk.js"
+  },
+  {
+    "revision": "801c7f5bdaba0c463e32",
+    "url": "/Firegram-Gallery-App/static/js/runtime-main.1d6a95ca.js"
+  }
+]);
